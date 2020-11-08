e51a5812978d303e3f21a71de1fffb2bf0666f92
Brown Mamba <brdyherrera@gmail.com>
2020-11-08 02:41:28 -0500

    Able to add task and stask type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
-
-    event.preventDefault();
-
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
-
-formEl.addEventListener("submit", createTaskHandler); {
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("select[name='task-type']").value;
+  var taskTypeInput = document.querySelector("input[name='task-name']").value;
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  //   listItemEl.textContent = taskNameInput;
+  //   listItemEl.textContent = taskTypeInput;
   tasksToDoEl.appendChild(listItemEl);
-}; 
 
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+  listItemEl.appendChild(taskInfoEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
